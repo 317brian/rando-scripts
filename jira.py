f207a4bbd912c881610936f292f48ad4868c23ae
brian-armory <53799971+brian-armory@users.noreply.github.com>
2021-07-27 16:30:54 -0700

    Update jira.py

--- jira.py
+++ jira.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-# If you clone the repo, I recommend using the script via a symlink:
-# ln -s /Users/brian/rando-scripts/jira.py /Users/brian/jira
+# Clone the repo or just copy the contents of this file to a python (.py) file. Update the URL on line 16 to the URL for your Jira instance.
+# Save it somewhere that's already in your PATH or add it to your PATH
+# Use it by running the following command: jira <key>-issue_number. For example jira doc-412
 
 import webbrowser, sys, pyperclip
 
