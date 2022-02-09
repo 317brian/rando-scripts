062a1928ae41004b72c420f7e19dadfd89529aac
brian-armory <brian.le@armory.io>
2022-02-08 20:31:04 -0800

    cleanup

--- github-open-modified-files.py
+++ github-open-modified-files.py
@@ -2,7 +2,7 @@
 
 import sys, pyperclip, os
 
-# Get the latest copy of the repo. Change master for main 
+# Get the latest copy of the repo.
 
 os.system("git checkout master || git checkout main")
 os.system("git pull --rebase")
@@ -21,6 +21,8 @@ for modified_file in f:
   print(modified_file)
   os.system(f"open {modified_file}")
 
+# close the file
+f.close()
 
 # delete file list
 os.remove("file_list.txt")
